53c4c887e80ffed77a66852b0435bc0a6aed49d4
Jianshen Liu <jliu120@ucsc.edu>
2019-12-14 18:44:05 -0800

    Fix typo

--- tests/rados-bench/scripts/avg_common.sh
+++ tests/rados-bench/scripts/avg_common.sh
@@ -6,7 +6,7 @@ function usage() {
   printf "usage: %s <file_pattern> [last_num_entries]
 
 file_pattern\\t\\t: find the files in which the last number of entries will be be calculated.
-            \\t\\t  this parameter should be quoted in any case.
+            \\t\\t  this parameter should be quoted in any cases.
 last_num_entries\\t: calculate the average value for the last number of entries (default 30).
 " "${BASH_SOURCE[0]}"
   exit 0
